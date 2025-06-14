0905ebcf31efe7dc4d3fa3f3dd6121a32da0ecaa
Vairamuthu <mvairamuthu2003@gmail.com>
2025-06-14 13:13:31 +0530

    fix: comment out Tokenomics navigation item in Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -101,30 +101,30 @@ function Navbar() {
       ),
       badge: "Join Waitlist",
     },
-    {
-      href: "/tokenomics",
-      label: "Tokenomics",
-      color: "text-[#ff00ff]",
-      hoverColor: "hover:text-[#00ffff]",
-      borderColor: "border-[#ff00ff]",
-      bgHoverColor: "hover:bg-[#ff00ff]/10",
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-          />
-        </svg>
-      ),
-    },
+    // {
+    //   href: "/tokenomics",
+    //   label: "Tokenomics",
+    //   color: "text-[#ff00ff]",
+    //   hoverColor: "hover:text-[#00ffff]",
+    //   borderColor: "border-[#ff00ff]",
+    //   bgHoverColor: "hover:bg-[#ff00ff]/10",
+    //   icon: (
+    //     <svg
+    //       xmlns="http://www.w3.org/2000/svg"
+    //       className="h-5 w-5"
+    //       fill="none"
+    //       viewBox="0 0 24 24"
+    //       stroke="currentColor"
+    //     >
+    //       <path
+    //         strokeLinecap="round"
+    //         strokeLinejoin="round"
+    //         strokeWidth="2"
+    //         d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+    //       />
+    //     </svg>
+    //   ),
+    // },
     {
       href: "#",
       label: "Chrome Extension (Soon)",
